c9b1f96130fc7f5b4e4948865027f7319eee4dfd
Antony Kuhl <akuhl@nevada.unr.edu>
2023-11-05 14:42:56 -0800

    Boss Room Added
    Boss Room Geometry Complete
    Gun Movement Updated to follow player model movement (In Need of Refinement)

--- Assets/WeaponMovement.cs
+++ Assets/WeaponMovement.cs
@@ -7,13 +7,23 @@ public class WeaponMovement : MonoBehaviour
     public Rigidbody2D rb;
     
     public Camera cam;
-    
+
+    public float moveSpeed = 10f;
+
     private Vector2 mousePos;
+
+    private Vector2 movement;
+
+    private GameObject playerObj = null;
 
     // Update is called once per frame
     void Update()
     {
+        playerObj = GameObject.Find("Player");
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        //Debug.Log("Player Position: X = " + playerObj.transform.position.x + " --- Y = " + playerObj.transform.position.y);
+        movement.x = playerObj.transform.position.x;
+        movement.y = playerObj.transform.position.y;
     }
 
     private void FixedUpdate()
@@ -21,5 +31,7 @@ public class WeaponMovement : MonoBehaviour
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+        rb.MovePosition(movement);
+
     }
 }
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -19,7 +19,6 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
